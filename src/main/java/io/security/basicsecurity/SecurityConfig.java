0f69f0b5cfe56f9f15de45f6ec48478befa5e5fb
weekbelt <vfrvfr4207@gmail.com>
2020-08-27 19:51:36 +0900

    4) Form Login 인증 필터: UsernamePasswordAuthenticationFilter

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -4,15 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -24,21 +15,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http
                 .formLogin()
-//                .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login")
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc")
-//                .successHandler((request, response, authentication) -> {
-//                    System.out.println("authentication : " + authentication.getName());
-//                    response.sendRedirect("/");
-//                })
-//                .failureHandler((request, response, exception) -> {
-//                    System.out.println("exception" + exception.getMessage());
-//                    response.sendRedirect("/login");
-//                })
-                .permitAll();
         ;
     }
 }
